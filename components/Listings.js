e94d3493e887c5c04f7d3dc9d9e16ae2d1613531
guzideiremyavuz <yavuzguzideirem@gmail.com>
2025-02-23 22:46:09 +0300

    kaydetme butonu ekledim

--- components/Listings.js
+++ components/Listings.js
@@ -1,8 +1,8 @@
-import { StyleSheet, Text, View, FlatList, Image } from "react-native";
+import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { ListingType } from "../types/listingType";
 import Colors, { primaryColor, black, white,bgColor } from "../constants/Colors";
-
+import { Ionicons} from  "@expo/vector-icons"
 
 const imageMap = {
   1: require("../assets/id1.jpg"),
@@ -20,25 +20,34 @@ const imageMap = {
 const Listings = ({ listings }) => {
   const renderItems = ({ item }) => {
     return (
+      <TouchableOpacity>
       <View className="flex-1 justify-center items-center px-6"
-      style={{backgroundColor:Colors.bgColor}}>
+      style={{backgroundColor:Colors.white}}>
         <Image
           source={imageMap[item.id]} 
           className="w-64 h-40 rounded-lg"
         />
-        <Text>{item.name}</Text>
+        <Text className="text-black text-xl font-bold">{item.name}</Text>
+        <View className="absolute top-[185px] right-[30px] p-2 rounded-full"
+        style={{backgroundColor: Colors.primaryColor,
+          borderWidth:2,
+          borderColor:Colors.white
+        }}>
+          <Ionicons name="bookmark-outline" size={24} color={Colors.white}/>
+        </View>
       </View>
+      </TouchableOpacity>
     );
   };
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <FlatList
+    <View className="flex-1  items-center justify-center">
+      <FlatList 
         data={listings}
         renderItem={renderItems}
         horizontal
         showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{ paddingHorizontal: 10 }}
+        contentContainerStyle={{ paddingHorizontal: 10,bottom:72 }}
         keyExtractor={(item) => item.id.toString()}
       />
     </View>
